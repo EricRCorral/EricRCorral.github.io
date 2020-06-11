759a27d5c7c3b6b86adbbc4e2bc32a6719b26014
EricRCorral <eric-wil@hotmail.com>
2020-06-11 17:11:01 -0300

    Third commit

---
+++ precache-manifest.4960d33aeffca2c8a4d55b8a324be92b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a6bb6998cb1d8497b5330e9b713eb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7eb79a8e729689f3eb7d",
+    "url": "/static/css/main.07533b95.chunk.css"
+  },
+  {
+    "revision": "b5d71a61997fe94744a0",
+    "url": "/static/js/2.36dda8b5.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/static/js/2.36dda8b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eb79a8e729689f3eb7d",
+    "url": "/static/js/main.394066dd.chunk.js"
+  },
+  {
+    "revision": "6d30a0bf2de0f0f8d263",
+    "url": "/static/js/runtime-main.21e74043.js"
+  },
+  {
+    "revision": "29d91e93cb6fb6e39b6804b6e07f6401",
+    "url": "/static/media/cry-of-fear.29d91e93.png"
+  },
+  {
+    "revision": "4fba121b79836df8b8b2b9a3a6b3b332",
+    "url": "/static/media/gourmetaurant.4fba121b.png"
+  },
+  {
+    "revision": "179469ee9f4a9bea7e746bd79039759e",
+    "url": "/static/media/images-firebase.179469ee.png"
+  },
+  {
+    "revision": "88af679a8aba26b9fe34e802e87409a2",
+    "url": "/static/media/storelist.88af679a.png"
+  },
+  {
+    "revision": "906f88f798636654680ec8f7842c5962",
+    "url": "/static/media/template-plan-ford-ka.906f88f7.png"
+  },
+  {
+    "revision": "0111a15d48d4285f92df045643816faf",
+    "url": "/static/media/tmdb.0111a15d.png"
+  }
+]);
